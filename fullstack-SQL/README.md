a8701a12d4a0af261639ae45efbe72aba9558598
Ashish Shah <eiepixxstuff@gmail.com>
2025-03-22 13:39:59 +0530

    full stack using prisma

--- fullstack-SQL/README.md
+++ fullstack-SQL/README.md
@@ -1,1 +1,7 @@
 [Prisma docs](https://www.prisma.io/docs/getting-started/prisma-postgres/from-the-cli)
+
+npx prisma format
+
+npx prisma migrate dev: to migrate the table into database
+
+npx prisma migrate
--- fullstack-SQL/controllers/auth.controller.js
+++ fullstack-SQL/controllers/auth.controller.js
@@ -1,9 +1,119 @@
-import { PrismaClient } from "@prisma/client"
-const Prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+import jwt from "jsonwebtoken"
 
-export const signup = async (req, res)=> {
-    console.log("Inside the controller")
-    await Prisma.user.findUnique({
-        where: {email}
-    })
-}
+const prisma = new PrismaClient();
+
+export const signup = async (req, res) => {
+    const { name, email, password, phone } = req.body;
+    if (!name || !email || !password || !phone) {
+        console.log('All fields are required.');
+        return res.status(400).json({
+            success: false,
+            message: 'Please provide all the fields.',
+        });
+    }
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (existingUser) {
+            return res.status(401).json({
+                success: false,
+                message: 'User already exist.',
+            });
+        }
+        // hash pass
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const verificationToken = crypto.randomBytes(32).toString('hex');
+        const user = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+                phone,
+                verificationToken,
+                phone,
+            },
+        });
+
+        //TODO: send mail functionality from other code file
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            error,
+            message: 'Internal server error.',
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(401).json({
+            success: false,
+            message: 'All fields are required.',
+        });
+    }
+
+    // check if user exist from this email or not
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Wrong email, please provide valid email address.',
+            });
+        }
+
+        //check password
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if(!isPasswordMatch){
+            return res.status(401).json({
+                success: false,
+                message: 'Wrong password, please provide correct password.',
+            });
+        }
+        
+        // create user login session
+        const jwtToken = jwt.sign(
+            {id: user.id, role: user.role},
+            process.env.JWT_SECRET_KEY,
+            {expiresIn: "24h"}
+        )
+
+
+        const cookieOptions = {
+            httpOnly: true
+        }
+        res.cookie("token", jwtToken, cookieOptions)
+
+        // email and password bhi correct then send 
+        res.status(200).json({
+            success: true,
+            message: "User login successfuly",
+           userdata: {
+                id: user.id, name: user.name,
+                email: user.email 
+            }
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'Login failed.',
+            error,
+        });
+    }
+};

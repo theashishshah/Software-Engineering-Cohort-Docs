9fb4497af4909e6e60823d64b37036de73419671
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-23 02:06:24 +0530

    learn: switching from react 18 to 19

--- React/01React/src/App.js
+++ React/01React/src/App.js
@@ -1,3 +1,6 @@
+import React from "https://esm.sh/react@19.1.0";
+import ReactDOM from "https://esm.sh/react-dom@19.1.0/client";
+
 const App = () => {
     return React.createElement(
         'div',

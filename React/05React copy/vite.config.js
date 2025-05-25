75b43c2e7b10c7a45ce52c23b7d323907467d91c
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-25 14:52:01 +0530

    vite config manually

---
+++ React/05React copy/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@src": path.resolve(__dirname, "src"),
+			"@components": path.resolve(__dirname, "src/components"),
+		},
+	},
+});
--- React/README.md
+++ React/README.md
@@ -4,6 +4,9 @@
 3. How hot module replacement works?
 4. transpile: esbuild or babel
 5. rollup for bundle code
+6. Babel
+7. esbuild
+
 
 # Ref links
 [bring react from cjs](https://unpkg.com/react@18.3.0-canary-ee68446ff-20231115/umd/react.development.js)

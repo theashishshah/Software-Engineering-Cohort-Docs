10da7e394a85c7cd1c36483726239f5093717d40
Ashish Shah <eiepixxstuff@gmail.com>
2025-06-04 23:56:37 +0530

    react application with tanstack router

---
+++ React/08React-Frameworkds/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{"display": "flex", "height": "100vh", "widhth": "100%"}}>
+      Hello from TanStack router
+    </div>
+  )
+}
+
+export default App
